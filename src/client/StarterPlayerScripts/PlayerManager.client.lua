e0cf1c756155d733aa58c4291c61c2ed60cfbdfe
Darkord2028 <98699507+Darkord2028@users.noreply.github.com>
2025-07-02 16:10:31 +0530

    Added Weapon Animation Event and Hitbox

--- src/client/StarterPlayerScripts/PlayerManager.client.lua
+++ src/client/StarterPlayerScripts/PlayerManager.client.lua
@@ -1,1 +1,14 @@
-print("Player Local Script")
+local UserInputService = game:GetService("UserInputService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Events = ReplicatedStorage:WaitForChild("Events")
+local attackEvent = Events:WaitForChild("AttackRequest")
+
+UserInputService.InputBegan:Connect(function(input, gameProcessed)
+	if gameProcessed then
+		return
+	end
+
+	if input.UserInputType == Enum.UserInputType.Keyboard and input.KeyCode == Enum.KeyCode.E then
+		attackEvent:FireServer()
+	end
+end)
