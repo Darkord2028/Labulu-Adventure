502dc4c70e3b90ddfa1e0051fb58362bea2d7cf8
Darkord2028 <98699507+Darkord2028@users.noreply.github.com>
2025-07-03 15:56:19 +0530

    Fixed Weapon Equip and Inventory

--- src/client/StarterPlayerScripts/PlayerManager.client.lua
+++ src/client/StarterPlayerScripts/PlayerManager.client.lua
@@ -1,17 +1,11 @@
 -- Game Services
-local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local UserInputService = game:GetService("UserInputService")
-
--- Local Player Variables
-local player = Players.LocalPlayer
+local WeaponModule = require(ReplicatedStorage:WaitForChild("Modules"):WaitForChild("WeaponModule"))
 
 -- Events
 local Events = ReplicatedStorage:WaitForChild("Events")
 local attackEvent = Events:WaitForChild("AttackRequest")
-local equipEvent = Events:WaitForChild("EquipWeaponRequest")
-
-local WeaponManager = require(ReplicatedStorage.Managers.WeaponManager)
 
 UserInputService.InputBegan:Connect(function(input, gameProcessed)
 	if gameProcessed then
