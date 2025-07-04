b355af71c54fc0b73f63cae982f5d121533cf7ac
Darkord2028 <98699507+Darkord2028@users.noreply.github.com>
2025-07-04 17:13:52 +0530

    Item uses adds up in the inventory

--- src/client/StarterPlayerScripts/PlayerManager.client.lua
+++ src/client/StarterPlayerScripts/PlayerManager.client.lua
@@ -1,18 +1,33 @@
 -- Game Services
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local UserInputService = game:GetService("UserInputService")
-local WeaponModule = require(ReplicatedStorage:WaitForChild("Modules"):WaitForChild("WeaponModule"))
 
 -- Events
 local Events = ReplicatedStorage:WaitForChild("Events")
-local attackEvent = Events:WaitForChild("AttackRequest")
+local attackEvent = Events:WaitForChild("AttackRequestEvent")
+local equipEvent = Events:WaitForChild("EquipWeaponEvent")
+local addItemEvent = Events:WaitForChild("AddItemEvent")
+
+-- Local Player Flags
+local isWeaponEquipped = false
 
 UserInputService.InputBegan:Connect(function(input, gameProcessed)
 	if gameProcessed then
 		return
 	end
 
-	if input.UserInputType == Enum.UserInputType.Keyboard and input.KeyCode == Enum.KeyCode.E then
+	if
+		input.UserInputType == Enum.UserInputType.Keyboard
+		and input.KeyCode == Enum.KeyCode.E
+		and not isWeaponEquipped
+	then
+		isWeaponEquipped = true
+		equipEvent:FireServer("TreeBranch")
+	end
+	if input.UserInputType == Enum.UserInputType.MouseButton1 and isWeaponEquipped then
 		attackEvent:FireServer()
+	end
+	if input.UserInputType == Enum.UserInputType.Keyboard and input.KeyCode == Enum.KeyCode.Q then
+		addItemEvent:FireServer("TreeBranch", 1)
 	end
 end)
