6283b6d75305f4a4cdd6459c2e8a0a4b22858bd0
Darkord2028 <98699507+Darkord2028@users.noreply.github.com>
2025-07-07 16:41:59 +0530

    Added Damage Pop Up

--- src/client/StarterPlayerScripts/PlayerManager.client.lua
+++ src/client/StarterPlayerScripts/PlayerManager.client.lua
@@ -2,11 +2,19 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local UserInputService = game:GetService("UserInputService")
 
+-- Player
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
 -- Events
 local Events = ReplicatedStorage:WaitForChild("Events")
 local attackEvent = Events:WaitForChild("AttackRequestEvent")
 local equipEvent = Events:WaitForChild("EquipWeaponEvent")
 local finishedAttackEvent = Events:WaitForChild("FinishedAttackEvent")
+local DisplayDamagePopUpEvent = Events:WaitForChild("DisplayDamagePopUpEvent")
+
+local DamagePopUpPoolModule = require(ReplicatedStorage:WaitForChild("Modules"):WaitForChild("DamagePopUpPoolModule"))
+local DamagePopUpPool = DamagePopUpPoolModule.new(20)
 
 -- Local Player Flags
 local isWeaponEquipped = false
@@ -18,7 +26,10 @@ end)
 
 equipEvent.OnClientEvent:Connect(function(weaponName)
 	isWeaponEquipped = true
-	print("Weapon equipped:", weaponName)
+end)
+
+DisplayDamagePopUpEvent.OnClientEvent:Connect(function(position, damage)
+	DamagePopUpPool:Display(position, damage)
 end)
 
 UserInputService.InputBegan:Connect(function(input, gameProcessed)
